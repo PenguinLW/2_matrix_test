2691c77350b5fbceca1aaa495a4ee906397bd19c
PenguinLW <diana1997525@gmail.com>
2025-03-22 23:51:55 +0800

    PenguinLW

--- release.sh
+++ release.sh
@@ -1,12 +1,22 @@
+#!/usr/bin/env bash
 #in venv
+#source ~/venv/bin/activate
 #pip install -r req.txt
-pip freeze > req.txt
-#git clone -o 2_matrix_test https://github.com/Kovalsky95/2_matrix_test.git
+#pip freeze > req.txt
+chmod u+x *.sh *.py
+l_dir=pwd
+
+git config --global --add safe.directory $l_dir
+#git clone -o 2_matrix_test https://github.com/PenguinLW/2_matrix_test.git
+#git config --global user.email "diana1997525@gmail.com"
+#git config --global user.name "PenguinLW"
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=25200"
+git config --global credential.helper "cache --timeout=8825200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
-git commit -m PenguinL
+git commit -m PenguinLW
 git push --set-upstream 2_matrix_test master
+#deactivate
